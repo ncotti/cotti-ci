bbf1095484a08622888ee200566056fb49271c6c
nicolas.cotti <nicolas.cotti@indiesemi.com>
2026-06-29 22:10:10 -0300

    Bats test

--- scripts/bats_test.sh
+++ scripts/bats_test.sh
@@ -13,22 +13,25 @@ source "${SCRIPT_DIR}/common.sh"
 ###############################################################################
 # Constants
 ###############################################################################
-MSG_INSTALL="Did not find shellcheck, installing..."
-MSG_RUNNING="Running shellcheck..."
-MSG_OK="Shell scripts OK."
+MSG_RUNNING="Running BATS tests..."
+MSG_OK="All tests finished successfully."
 
 MSG_USAGE=$(cat <<EOF
 ${BOLD_MAGENTA}Usage:${NC}
     ${0} <target_dir>
 
 Where:
-* ${BOLD}${UNDERLINE}<target_dir>${NC}: Root directory from where all ".sh" files will be evaluated.
+* ${BOLD}${UNDERLINE}<target_dir>${NC}: Directory where the bats tests are.
 EOF
 )
 
-MSG_NO_TARGET_DIR="No target directory argument."
+MSG_WRONG_USAGE="Wrong or missing parameters."
 
 MSG_TARGET_DIR_IS_NOT_A_DIR="target_dir is not a directory: %s"
+
+MSG_NO_TESTS_FOUND="target_dir: \"%s\" is valid, but there are no tests in it."
+
+MSG_INSTALL_FAIL="Couldn't find BATS as a submodule in your repo."
 
 ###############################################################################
 # Arguments and argument validation
@@ -36,7 +39,7 @@ MSG_TARGET_DIR_IS_NOT_A_DIR="target_dir is not a directory: %s"
 target_dir="$1"
 
 if [ -z "${target_dir}" ]; then
-    error "${MSG_NO_TARGET_DIR}"
+    error "${MSG_WRONG_USAGE}"
     printf "${MSG_USAGE}\n"
     exit 1
 fi
@@ -46,19 +49,41 @@ if [ ! -d "${target_dir}" ]; then
     exit 1
 fi
 
+# Check if there are at least one valid ".bats" test file in the target dir
+if ! compgen -G "${target_dir}/*.bats" >/dev/null; then
+    error "${MSG_NO_TESTS_FOUND}" "${target_dir}"
+    exit 1
+fi
+
 ###############################################################################
 # Tool installation
 ###############################################################################
-if ! command -v shellcheck &>/dev/null; then
-    warning "${MSG_INSTALL}"
-    sudo apt install shellcheck
+# Try to find the submodules for bats
+BATS=""
+if ! which bats; then
+    bats_submodules="$(cd "${target_dir}" && git submodule status | grep 'bats' | awk '{print $2}')"
+    if [ -n "${bats_submodules}" ]; then
+        #shellcheck disable=SC2086
+        cd "${target_dir}" && git submodule update --init ${bats_submodules}
+
+        for submodule in ${bats_submodules}; do
+            if [ -x "${target_dir}/${submodule}/bin/bats" ]; then
+                BATS="${target_dir}/${submodule}/bin/bats"
+            fi
+        done
+    fi
+else
+    BATS=$(which bats)
+fi
+
+if [ -z "${BATS}" ]; then
+    error "${MSG_INSTALL_FAIL}"
+    exit 1
 fi
 
 ###############################################################################
 # Script
 ###############################################################################
 info "${MSG_RUNNING}"
-find "${target_dir}" -type f \
-    \( -name "*.sh" -o -name "*.bash" -o -name "*.bats" \) \
-    -exec shellcheck --format=tty --severity=style -x {} +
+"${BATS}" "${target_dir}"
 ok "${MSG_OK}"
